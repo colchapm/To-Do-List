33ebbfa43fec02f4c88843a2226d889979d1d867
Collin Chapman <cchap14@gmail.com>
2021-04-17 13:37:57 -0700

    create first two passing tests - get data type of item class and items can hold simple descriptions

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -6,6 +6,24 @@ namespace ToDoList.Tests
   [TestClass]
   public class ItemTest
   {
-    
+
+    [TestMethod]
+    public void ItemConstructor_CreatesInstanceOfItem_Item()
+    {
+      Item newItem = new Item("test");
+      Assert.AreEqual(typeof(Item), newItem.GetType());
+    }
+
+    [TestMethod]
+    public void GetDescription_ReturnsDescription_String()
+    {
+      //Arrange
+      string description = "Walk the dog.";
+      Item newItem = new Item(description);
+      //Act
+      string result = newItem.Description;
+      //Assert
+      Assert.AreEqual(description, result);
+    }
   }
 }
