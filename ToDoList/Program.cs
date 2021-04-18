b4d034fab342bc050492ac097195d54a388ba932
Collin Chapman <cchap14@gmail.com>
2021-04-18 10:56:36 -0700

    add user interface - ability to add or view the to do list
    Co-authored-by: Salim Mayan <mailsalim@gmail.com>

---
+++ ToDoList/Program.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using ToDoList.Models;
+
+namespace ToDoList
+{
+  public class Program
+  {
+    
+    public static void Main()
+    {
+      Greeting();
+      static void Greeting()
+      {
+        Console.WriteLine("Welcome to the To-Do List");
+        AddOrViewList();
+      }
+      static void AddOrViewList()
+      {
+        Console.WriteLine("Would you like to add an item to your list or view your list? (Add/View)");
+        string answer = Console.ReadLine();
+
+        if (answer == "Add" || answer == "add")
+        {
+          Console.WriteLine("Please enter the description for the new item");
+          string response = Console.ReadLine();
+          Item newItem = new Item(response);
+          Console.WriteLine(response + " has been added to your list");
+          AddOrViewList();
+        }
+        else
+        {
+          List<Item> completeList = new List<Item>() { };
+          completeList = Item.GetAll();
+          if (completeList.Count == 0) {
+            Console.WriteLine("Your list is empty, add a task first.");
+            AddOrViewList();
+          } else
+          {
+            foreach (var element in completeList)
+            {
+              Console.WriteLine("An item on your to do list is " + element.Description);
+            }
+            AddOrViewList();
+          }
+        }
+      }
+    }
+  }
+}
