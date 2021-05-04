b12971bedd6e1e90df5a869eec3ef291104dacd4
Collin Chapman <cchap14@gmail.com>
2021-05-03 17:58:51 -0700

    add a show route and modify the Create form route to New

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -16,7 +16,7 @@ namespace ToDoList.Controllers
     }
 
     [HttpGet("/items/new")]
-    public ActionResult CreateForm()
+    public ActionResult New()
     {
       return View();
     }
@@ -34,5 +34,13 @@ namespace ToDoList.Controllers
       Item.ClearAll();
       return View();
     }
+
+    [HttpGet("/items/{id}")]
+    public ActionResult Show(int id)
+    {
+      Item foundItem = Item.Find(id);
+      return View(foundItem);
+    }
+
   }
 }
--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -5,6 +5,7 @@ namespace ToDoList.Models
   public class Item
   {
     public string Description { get; set; }
+    public int Id { get; }
     public int Priority { get; set; }
 
     private static List<Item> _instances = new List<Item> {};
@@ -14,6 +15,7 @@ namespace ToDoList.Models
     {
       Description = description;
       _instances.Add(this);
+      Id = _instances.Count;
     }
     public Item(string description, int priority)
       : this(description)
@@ -29,6 +31,11 @@ namespace ToDoList.Models
     public static void ClearAll()
     {
       _instances.Clear();
+    }
+
+    public static Item Find(int searchId)
+    {
+      return _instances[searchId-1];
     }
   }
 }
