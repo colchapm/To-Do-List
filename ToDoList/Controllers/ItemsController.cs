597404ef86c47e52537dbabb3532ec1a58c7e7ab
Collin Chapman <cchap14@gmail.com>
2021-05-03 17:33:54 -0700

    add POST method to delete list items

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -28,6 +28,11 @@ namespace ToDoList.Controllers
       return RedirectToAction("Index");
     }
 
-
+    [HttpPost("/items/delete")]
+    public ActionResult DeleteAll()
+    {
+      Item.ClearAll();
+      return View();
+    }
   }
 }
